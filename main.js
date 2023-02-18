f6020f8f2fdda581fcc9cacd1e7b2b1019a5a25b
Пользователь <TokhirovIlhom>
2023-02-18 22:11:35 +0500

    JavaScript.3-dars

---
+++ main.js
@@ -0,0 +1,161 @@
+//1-misol
+
+// var k = 1
+// var n = 10
+
+// for (var i = 0; i < n; i++){
+//     console.log(k);
+// }
+
+//2-misol
+
+// var a = 1
+// var b = 10
+
+// for (var i = a; i <= b; i++ ){
+//     console.log(i);
+// }
+
+//3-misol
+
+// var a = 10
+// var b = 1
+
+// for (var i = a -1; i > b; i-- ){
+//     console.log(i);
+// }
+
+//4-misol
+
+// var konfet = 10000
+// var limit = 10
+
+// for(var i = 0; i <= limit; i++){
+//     let natija = i * konfet
+// }
+
+
+//5-misol
+
+// var konfet = 10000
+// var limit = 1
+
+// for(var i = 0; i <= limit; i++){
+//     let natija = i * konfet
+// }
+
+
+//6-misol
+
+
+//7-misol
+//hato !
+
+// var a = 1
+
+// var b = 10
+
+// let sum = 0
+
+// for (var i = a; i <= 10; i++ ){
+
+//     console.log(sum);
+
+//     sum *= i
+// }
+
+//8-misol
+
+// var a = 1
+
+// var b = 10
+
+// let sum = 1
+
+// for (var i = a; i <= 10; i++ ){
+
+//     console.log(sum);
+
+//     sum *= i
+// }
+
+
+//9-misol
+
+
+
+
+//10-misol
+
+// let num = 18
+
+// for (let i = 0; i < num; i++) {
+//     if (i / 1) {
+//         console.log(i++);
+//     }
+// }
+
+
+//11-misol
+
+
+// var n = 20
+
+// for (i = 0; i < n; i++){
+//     // console.log(i);
+//     if (i % 2 != 0){
+//         console.log(i);
+//     }
+// }
+
+
+// 12-misol
+
+
+// var n = 20
+
+// for (i = 1; i < n; i++){
+//     // console.log(i);
+//     if (i % 2 == 0){
+//         console.log(i);
+//     }
+// }
+
+
+
+//13-misol
+
+
+// var n = 20
+
+// for (i / i === 1 && i % i / 1 == i2 === 0 && 2 === 0 ) {
+//     console.log(i);
+// }
+
+//14-misol
+
+
+// var juft = []
+// var toq = []
+
+// var n = 12
+
+// for (var i = 0; i <=n; i++ ){
+//     if (i % 2 === 0) {
+//         juft.push(i)
+//     } else {
+//         toq.push(i)
+//     }
+// }
+
+
+//16-misol
+
+
+// let num = 50
+
+// for (let i = 0; i <= num; i++){
+//     if (i % 5){
+//         console.log(i);
+//     }
+// }
